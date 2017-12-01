45911a1053d021f6764611412c3c65914cfee6e5
Ahmed <ahmed_nasser@outlook.com>
2017-12-01 22:07:59 +0200

    change (sum) to (summation) and correct typo

--- checksum.py
+++ checksum.py
@@ -1,11 +1,11 @@
 def udp_checksum(data):
-    pos = len(data)
-    if (pos % 2 != 0):  # If odd...
-        pos -= 1
-        sum = ord(data[pos])  # initialize the sum with the odd end byte
+    size = len(data)
+    if (size % 2 != 0):  # If odd...
+        size -= 1
+        summation = ord(data[size])  # initialize the sum with the odd end byte
     else:
-        sum = 0
-    for i in range(0, len(msg), 2):
-        sum += (ord(data[pos + 1]) << 8) + ord(data[pos])
-    sum = (sum >> 16) + (sum & 0xffff)
-    return (~sum) & 0xffff
+        summation = 0
+    for i in range(0, size, 2):
+        summation += (ord(data[i + 1]) << 8) + ord(data[i])
+    summation = (summation >> 16) + (summation & 0xffff)
+    return (~summation) & 0xffff
